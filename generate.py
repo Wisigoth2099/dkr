7c20de9f6c5f1d7061a645bd301b7be05657de74
Wisigoth2099 <74539722+Wisigoth2099@users.noreply.github.com>
2022-05-02 15:58:59 +0200

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LinearRegression
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+X_TEST = [[10, 20, 30]]
+outcome = predictor.predict(X=XTEST)
+coefficients = predictor.coef
+
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
+with open('input.data.json') as f:
+  content=f.read()
+  TRAIN_INPUT=loads(content)
+with open('output.data.json') as f:
+  content=f.read()
+  TRAIN_OUTPUT=loads(content)
